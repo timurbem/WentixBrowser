9b8fbb0b506d4a6cbc03506c14a66bb32dcab1c2
timurbem <timurvlasenko22@gmail.com>
2026-03-22 14:04:39 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wentix.browser"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.wentix.browser"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures { viewBinding = true }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+}
